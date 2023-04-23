6f5987d47ada68d29be87f589c574cd89e1f7924
Archisman Tripathi <archismantripathi@gmail.com>
2023-04-23 22:05:34 +0530

    fix router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  { path: 'login',     component: LoginComponent     },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '**',        redirectTo: '/dashboard'      }
+  { path: 'login',     component: LoginComponent},
+  { path: 'home',      component: MainComponent },
+  { path: '**',        redirectTo: '/home'      }
 ];
 
 @NgModule({
